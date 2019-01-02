3fd85605ae8b144c1e7d1709c773a218470346f5
Veres Roland <roli25veres@gmail.com>
2019-01-02 20:28:40 +0200

    Properties

--- POO/Proprietati.cs
+++ POO/Proprietati.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace POO
 {
-    class Proprietati
+    class robot
     {
+        string name;
+        int type;
+        public  robot(string name, int type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+        public  robot()
+        {
+
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+        public void Present()
+        {
+            Console.WriteLine($"Hey, I am {name} and I am a type {type} robot.");
+        }
+    }
+    class Proprietati
+    { 
+        static void Main()
+        {
+            robot jasper = new robot("Jasper", 32);
+            jasper.Present();
+            robot mycin = new robot();
+            mycin.Name = "Mycin";
+            mycin.Type = 12;
+            mycin.Present();
+            Console.ReadKey();
+        }
+
     }
 }
